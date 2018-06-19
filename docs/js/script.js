69287238115752aa929f05ff142a237febd20054
nastya-randall <nastya-randall@users.noreply.github.com>
2018-06-19 18:00:28 +0200

    script for navigation (mobile)

---
+++ docs/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var navMain = document.querySelector('.main-nav');
+var toggleOpen = document.querySelector('.main-nav__open');
+var toggleClose = document.querySelector('.main-nav__close');
+
+navMain.classList.remove('main-nav--nojs');
+
+toggleOpen.addEventListener('click', function() {
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  });
+
+toggleClose.addEventListener('click', function () {
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened');
+});
+
