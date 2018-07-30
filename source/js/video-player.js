e5ba8ea423376394868cae6ff13b75b06037bf9f
nastya-randall <nastya-randall@users.noreply.github.com>
2018-07-30 18:02:46 +0200

    js

--- source/js/video-player.js
+++ source/js/video-player.js
@@ -1,63 +1,11 @@
 'use strict';
 (function () {
 
-  var clamp = function (num, min, max) {
-    return Math.min(Math.max(num, min), max);
-  };
-
-  var thumb = document.querySelector('.video__thumb');
-  var bar = document.querySelector('.video__bar');
-  var THUMB_WIDTH = thumb.offsetWidth;
-
-  thumb.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-
-    var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-    };
-
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      thumb.style.left = clamp(thumb.offsetLeft - shift.x, bar.offsetLeft, bar.offsetLeft + bar.offsetWidth - THUMB_WIDTH) + 'px';
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
-  var playerBar = document.querySelector('.video__player-bar');
-
-  playerBar.addEventListener('click', function (evt) {
-    evt.preventDefault();
-
-    var barCoords = bar.getBoundingClientRect();
-
-    thumb.style.left = evt.clientX + bar.offsetLeft - barCoords.x - THUMB_WIDTH / 2 + 'px';
-  });
-
+  var videoSlider = document.querySelector('.video__range-slider');
   var replayButton = document.querySelector('.video__button--replay');
 
   replayButton.addEventListener('click', function () {
-    thumb.style.left = 0;
+    videoSlider.value = 0;
   });
 
 
@@ -65,12 +13,14 @@
   var isLiked = false;
   var likesCol = document.querySelectorAll('.gallery__likes');
 
-  for (var i =0; i < likesCol.length; i++) {
-    var icon = likesCol[i].querySelector('.gallery__likes-icon');
-    var likesNumber = likesCol[i].querySelector('.gallery__likes-number');
+  for (var i = 0; i < likesCol.length; i++) {
+//    var icon = likesCol[i].querySelector('.gallery__likes-icon');
 
-    icon.addEventListener('click', function () {
-      likesNumber.innerHTML = 1 + likesNumber.innerHTML;
+    likesCol[i].addEventListener('click', function (evt) {
+      console.log(evt.target);
+      var likesNumber = evt.target.querySelector('.gallery__likes-number');
+      console.log(likesNumber);
+      likesNumber.innerHTML = 1 + parseInt(likesNumber.innerHTML, 10);
     })
   }
 
